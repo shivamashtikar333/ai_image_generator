7da34e458d8ce1c3efaac29079b2e4e2443bf263
shivamashtikar333 <shivamaace121@gst.sies.edu.in>
2024-06-26 15:24:11 +0530

    backend url fixed

--- client/src/Pages/CreatePost.jsx
+++ client/src/Pages/CreatePost.jsx
@@ -28,7 +28,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch('https://ai-image-generator-ib20.onrender.com//api/v1/kaleidoscope', {
+        const response = await fetch('https://ai-image-generator-ib20.onrender.com/api/v1/kaleidoscope', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
@@ -56,7 +56,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setLoading(true);
       try {
-        const response = await fetch('https://ai-image-generator-ib20.onrender.com//api/v1/post', {
+        const response = await fetch('https://ai-image-generator-ib20.onrender.com/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -26,7 +26,7 @@ const Home = () => {
     setLoading(true);
 
     try {
-      const response = await fetch('https://ai-image-generator-ib20.onrender.com//api/v1/post', {
+      const response = await fetch('https://ai-image-generator-ib20.onrender.com/api/v1/post', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
